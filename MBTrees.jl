71ae43c986768e05d6b0e63c78d45237942e9035
AJTrowbridge <aaron.j.trowbridge@gmail.com>
2020-04-20 08:41:03 -0400

    refactor(lattice-type): implemnted on correct branch this time

--- MBTrees.jl
+++ MBTrees.jl
@@ -18,28 +18,27 @@ mutable struct MBTree
     r::Union{MBTree, Nothing}
     leaf::Bool
     flip::Bool
-    T::Float64
     N::Int64
     type::Symbol
 
-    MBTree(T::Float64, N::Int64, type::Symbol) = new(0.0, 0.0,
-                                                     Vector{Int64}(undef, 0),
-                                                     Vector{Int64}(undef, 0),
-                                                     nothing, nothing, nothing,
-                                                     true, false,
-                                                     T, N, type)
+    MBTree(N::Int64, type::Symbol) = new(0.0, 0.0,
+                                         Vector{Int64}(undef, 0),
+                                         Vector{Int64}(undef, 0),
+                                         nothing, nothing, nothing,
+                                         true, false,
+                                         N, type)
 end
 
-function build_tree(bs, T, type)
-    N = size(bs)[1]
-    tree = MBTree(T, N, type)
+function build_tree(l::SpinLattice, type::Symbol)
+    N = l.N
+    tree = MBTree(l.N, type)
     if type == :tri
-        for b in bs
-            populate_trinary!(tree, b)
+        for b in l.bs
+            populate_trinary!(tree, b, l.T)
         end
     elseif type == :bi
-        for b in bs
-            populate_binary!(tree, b)
+        for b in l.bs
+            populate_binary!(tree, b, l.T)
         end
     end
     tree
@@ -47,8 +46,9 @@ end
 
 m(ΔE, T) = minimum([1, exp(-ΔE / T)])
 
-function populate_trinary!(t, b)
-    t.sum += m(2*b.E, t.T)
+function populate_trinary!(t::MBTree, b::SpinBody, T::Float64)
+    p = m(2*b.E, T)
+    t.sum += m(2*b.E, T)
     if t.b == nothing
         t.b = b
     else
@@ -56,20 +56,48 @@ function populate_trinary!(t, b)
         if rand(Bool)
             push!(t.lks, b.k)
             if t.l == nothing
-                t.l = MBTree(t.T, t.N, t.type)
+                t.l = MBTree(t.N, t.type)
             end
-            populate_trinary!(t.l, b)
+            populate_trinary!(t.l, b, T)
         else
             push!(t.rks, b.k)
             if t.r == nothing
-                t.r = MBTree(t.T, t.N, t.type)
+                t.r = MBTree(t.N, t.type)
             end
-            populate_trinary!(t.r, b)
+            populate_trinary!(t.r, b, T)
         end
     end
 end
 
-function choose_body_trinary(t, x)
+function populate_binary!(t::MBTree, b::SpinBody, T::Float64)
+    p = m(2*b.E, T)
+    t.sum += p
+    if t.b == nothing
+        if t.leaf
+            t.b = b
+        else
+            if rand(Bool)
+                push!(t.lks, b.k)
+                t.cut += p
+                populate_binary!(t.l, b, T)
+            else
+                push!(t.rks, b.k)
+                populate_binary!(t.r, b, T)
+            end
+        end
+    else
+        push!(t.lks, t.b.k)
+        push!(t.rks, b.k)
+        t.l = MBTree(t.N, t.type)
+        t.r = MBTree(t.N, t.type)
+        populate_binary!(t.l, t.b, T)
+        populate_binary!(t.r, b, T)
+        t.b = nothing
+        t.leaf = false
+    end
+end
+
+function choose_body_trinary(t::MBTree, x::Float64)
     if t.leaf
         return (t.b.i, t.b.j)
     else
@@ -99,35 +127,7 @@ function choose_body_trinary(t, x)
     end
 end
 
-function populate_binary!(t, b)
-    p = m(2*b.E, t.T)
-    t.sum += p
-    if t.b == nothing
-        if t.leaf
-            t.b = b
-        else
-            if rand(Bool)
-                push!(t.lks, b.k)
-                t.cut += p
-                populate_binary!(t.l, b)
-            else
-                push!(t.rks, b.k)
-                populate_binary!(t.r, b)
-            end
-        end
-    else
-        push!(t.lks, t.b.k)
-        push!(t.rks, b.k)
-        t.l = MBTree(t.T, t.N, t.type)
-        t.r = MBTree(t.T, t.N, t.type)
-        populate_binary!(t.l, t.b)
-        populate_binary!(t.r, b)
-        t.b = nothing
-        t.leaf = false
-    end
-end
-
-function choose_body_binary(t, x)
+function choose_body_binary(t::MBTree, x::Float64)
     if t.leaf
         return (t.b.i, t.b.j)
     else
@@ -139,48 +139,54 @@ function choose_body_binary(t, x)
     end
 end
 
-function freeman_step!(bs, tree)
+function freeman_step!(l::SpinLattice, tree::MBTree)
     x = rand(0:1e-10:tree.sum)
     if tree.type == :tri
-        (i, j) = choose_body_trinary(tree,x)
+        (i, j) = choose_body_trinary(tree, x)
     else
         (i, j) = choose_body_binary(tree, x)
     end
-    Ei = bs[i,j].E
+    Ei = l.bs[i,j].E
     Ef = -Ei
     ΔE = Ef - Ei
-    α = m(ΔE, tree.T)
+    α = m(ΔE, l.T)
     u = rand()
     if u < α
-        bs[i,j].s *= -1
-        tree_update_energies!(tree, bs, i, j)
+        l.bs[i,j].s *= -1
+        tree_update_energies!(tree, l, i, j)
         tree.flip = true
+        l.flips += 1
     end
 end
 
 k(i, j, N) = (i - 1)*N + j
 
-function tree_update_energies!(tree, bs, i, j)
-    N = tree.N
-    I = [i, mod(i, N) + 1, i,   mod(i-2, N) + 1, i]
-    J = [j, j,   mod(i, N) + 1, j,   mod(j-2, N) + 1]
+function tree_update_energies!(tree::MBTree, l::SpinLattice, i::Int64, j::Int64)
+    N = l.N
+    l.bs[i,j].E *= -1
+    I = [mod(i, N) + 1, i,   mod(i-2, N) + 1, i]
+    J = [j,   mod(i, N) + 1, j,   mod(j-2, N) + 1]
     qs = collect(zip(I, J))
-    update_energies!(bs, qs)
+
+    update_energies!(l, qs)
 
     ks = [k(i, j, N) for (i, j) in qs]
-    ΔEs = energy_shifts(bs, qs)
+    ΔEs = energy_shifts(l.bs, qs)
+
     if tree.type == :tri
         for (k, ΔE) in zip(ks, ΔEs)
-            update_trinary!(tree, k, ΔE)
+            p = m(ΔE, l.T)
+            update_trinary!(tree, k, p, ΔE, l.T)
         end
     else
         for (k, ΔE) in zip(ks, ΔEs)
-            update_binary!(tree, k, ΔE)
+            p = m(ΔE, l.T)
+            update_binary!(tree, k, p, ΔE, l.T)
         end
     end
 end
 
-function energy_shifts(bs, qs)
+function energy_shifts(bs::Matrix{SpinBody}, qs::Vector{Tuple{Int64,Int64}})
     N = size(bs)[1]
     ΔEs = []
     for (i, j) in qs
@@ -197,31 +203,30 @@ function energy_shifts(bs, qs)
     ΔEs
 end
 
-function update_trinary!(t, k, ΔE)
-    p = m(ΔE, t.T)
+function update_trinary!(t::MBTree, k::Int64, p::Float64, ΔE::Float64, T::Float64)
     t.sum += p
     if t.b.k == k
         t.b.E += ΔE
     else
         if k in t.lks
-            update_trinary!(t.l, k, ΔE)
+            update_trinary!(t.l, k, p, ΔE, T)
         else
-            update_trinary!(t.r, k, ΔE)
+            update_trinary!(t.r, k, p, ΔE, T)
         end
     end
 end
 
-function update_binary!(t, k, ΔE)
-    p = m(ΔE, t.T)
+
+function update_binary!(t::MBTree, k::Int64, p::Float64, ΔE::Float64, T::Float64)
     t.sum += p
     if t.leaf
         t.b.E += ΔE
     else
         if k in t.lks
             t.cut += p
-            update_binary!(t.l, k, ΔE)
+            update_binary!(t.l, k, p, ΔE, T)
         else
-            update_binary!(t.r, k, ΔE)
+            update_binary!(t.r, k, p, ΔE, T)
         end
     end
 end
--- SpinBodies.jl
+++ SpinBodies.jl
@@ -4,7 +4,9 @@
 
 module SpinBodies
 
-export SpinBody, spin_lattice, update_energies!, metropolis_step!
+using Statistics
+
+export SpinBody, SpinLattice, update_lattice!, update_energies!, metropolis_step!
 
 mutable struct SpinBody
     s::Int64
@@ -16,29 +18,51 @@ mutable struct SpinBody
     SpinBody(i::Int64, j::Int64, N::Int64) = new(rand([-1,1]), 0.0, i, j, k(i, j, N))
 end
 
+k(i, j, N) = (i - 1)*N + j
+
 mutable struct SpinLattice
+    N::Int64
     bs::Matrix{SpinBody}
     T::Float64
     M::Float64
     E::Float64
     c::Float64
+    flips::Int64
 
     function SpinLattice(N::Int64, T::Float64)
         bs = Matrix{SpinBody}(undef, N, N)
+
         for i = 1:N, j = 1:N
             bs[i,j] = SpinBody(i, j, N)
         end
+
         init_energy!(bs, N)
 
-        new(bs, T, 0, 0, 0)
+        M = avg_magnetization(bs)
+        E = avg_energy(bs)
+        c = specific_heat(bs, T)
+
+        new(N, bs, T, M, E, c, 0)
     end
 end
 # TO-DO: add methods for correlation function
 
-k(i, j, N) = (i - 1)*N + j
+avg_magnetization(bs::Matrix{SpinBody}) =
+    mean([b.s for b in bs])
 
-function init_energy!(bs::Matrix{SpinBody})
-    N = size(bs)[1]
+avg_energy(bs::Matrix{SpinBody}) =
+    mean([b.E for b in bs])
+
+specific_heat(bs::Matrix{SpinBody}, T::Float64) =
+    (mean([b.E^2 for b in bs]) - mean([b.E for b in bs])^2) / T^2
+
+function update_lattice!(l::SpinLattice)
+    l.E = avg_energy(l.bs)
+    l.M = avg_magnetization(l.bs)
+    l.c = specific_heat(l.bs, l.T)
+end
+
+function init_energy!(bs::Matrix{SpinBody}, N::Int64)
     for i = 1:N, j = 1:N
         sk = bs[i, j].s
         sr = bs[i, mod(j, N) + 1].s
@@ -49,56 +73,42 @@ function init_energy!(bs::Matrix{SpinBody})
     end
 end
 
-# function avg_energy(l)
-
-function spin_lattice(N)
-    bs = Matrix{SpinBody}(undef, N, N)
-    for i = 1:N, j = 1:N
-        bs[i,j] = SpinBody(i, j, N)
-    end
-    init_energy!(bs)
-    bs
-end
-
-function update_adj_energies!(bs, i, j)
-    N = size(bs)[1]
+function update_energies!(l::SpinLattice, i::Int64, j::Int64)
+    N = l.N
+    l.bs[i,j].E *= -1
     I = [mod(i, N) + 1, i,   mod(i-2, N) + 1, i]
     J = [j,   mod(i, N) + 1, j,   mod(j-2, N) + 1]
     qs = collect(zip(I, J))
-    update_energies!(bs, qs)
+    update_energies!(l, qs)
 end
 
-function update_energies!(bs, qs)
-    N = size(bs)[1]
+function update_energies!(l::SpinLattice, qs::Vector{Tuple{Int64, Int64}})
+    N = l.N
     for (i, j) in qs
-        sk = bs[i, j].s
-        sr = bs[i, mod(j, N) + 1].s
-        su = bs[mod(i, N) + 1, j].s
-        sl = bs[i, mod(j-2, N) + 1].s
-        sd = bs[mod(i-2, N) + 1, j].s
-        bs[i,j].E = -sk * (sr + su + sl + sd)
+        sk = l.bs[i, j].s
+        sr = l.bs[i, mod(j, N) + 1].s
+        su = l.bs[mod(i, N) + 1, j].s
+        sl = l.bs[i, mod(j-2, N) + 1].s
+        sd = l.bs[mod(i-2, N) + 1, j].s
+        l.bs[i,j].E = -sk * (sr + su + sl + sd)
     end
 end
 
 m(ΔE, T) = minimum([1, exp(-ΔE / T)])
 
-function metropolis_step!(bs, T)
-    flip = false
-    N = size(bs)[1]
+function metropolis_step!(l::SpinLattice)
+    N = l.N
     i, j = rand(1:N), rand(1:N)
-    Ei = bs[i,j].E
+    Ei = l.bs[i,j].E
     Ef = -Ei
     ΔE = Ef - Ei
-    α = m(ΔE, T)
+    α = m(ΔE, l.T)
     u = rand()
     if u < α
-        bs[i,j].s *= -1
-        bs[i,j].E = Ef
-        update_adj_energies!(bs, i, j)
-        flip = true
+        l.bs[i,j].s *= -1
+        update_energies!(l, i, j)
+        l.flips += 1
     end
-    (bs, flip)
 end
-
 
 end
