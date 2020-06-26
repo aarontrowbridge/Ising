872dd15500cecdf5f512bf55df32f18bddcc7ae2
AJTrowbridge <aaron.j.trowbridge@gmail.com>
2020-06-26 01:46:56 -0400

    bug-fixes: fast method successfully implemented!!!

--- SpinBodies.jl
+++ SpinBodies.jl
@@ -6,7 +6,8 @@ module SpinBodies
 
 using Statistics
 
-export SpinBody, SpinLattice, visualize, update_lattice!, metropolis_step!
+export SpinBody, SpinLattice
+export visualize, update_energies!, update_energies_fast!, update_lattice!, metropolis_step!
 
 mutable struct SpinBody
     s::Int
@@ -16,7 +17,7 @@ mutable struct SpinBody
     k::Int
 
     SpinBody(i::Int, j::Int, N::Int) = new(rand([-1,1]), 0, i, j, k(i, j, N))
-    SpinBody(s::Int, i::Int, j::Int, N::Int) = new(s, 0, i, j, k(i, j, N))
+    SpinBody(s::Int, E::Int, i::Int, j::Int, k::Int) = new(s, E, i, j, k)
 end
 
 k(i, j, N) = (i - 1)*N + j
@@ -28,6 +29,7 @@ mutable struct SpinLattice
     M::Float64
     E::Float64
     c::Float64
+    steps::Int
     flips::Int
 
     function SpinLattice(N::Int, T::Float64)
@@ -39,16 +41,16 @@ mutable struct SpinLattice
         M = avg_magnetization(bs)
         E = avg_energy(bs)
         c = specific_heat(bs, T)
-        new(N, bs, T, M, E, c, 0)
+        new(N, bs, T, M, E, c, 0, 0)
     end
 
     SpinLattice(N::Int, bs::Matrix{SpinBody}, T::Float64, M::Float64, E::Float64, c::Float64) =
-        new(N, bs, T, M, E, c, 0)
+        new(N, bs, T, M, E, c, 0, 0)
 end
 
 # TO-DO: add methods for correlation function
 
-Base.copy(b::SpinBody, N::Int) = SpinBody(b.s, b.i, b.j, N)
+Base.copy(b::SpinBody, N::Int) = SpinBody(b.s, b.E, b.i, b.j, b.k)
 
 Base.copy(bs::Matrix{SpinBody}, N::Int) = begin
     bs_copy = Matrix{SpinBody}(undef, N, N);
@@ -93,14 +95,18 @@ function init_energy!(bs::Matrix{SpinBody}, N::Int)
     end
 end
 
-function update_energies!(L::SpinLattice, i::Int, j::Int)
+function flip!(L::SpinLattice, i::Int, j::Int; fast=false)
     L.bs[i,j].s *= -1
     L.bs[i,j].E *= -1
     N = L.N
     I = [mod(i, N) + 1, i, mod(i-2, N) + 1, i]
     J = [j, mod(j, N) + 1, j, mod(j-2, N) + 1]
     qs = collect(zip(I, J))
-    update_energies!(L, qs)
+    if fast
+        update_energies_fast!(L, qs, L.bs[i,j].s)
+    else
+        update_energies!(L, qs)
+    end
 end
 
 function update_energies!(L::SpinLattice, qs::Vector{Tuple{Int,Int}})
@@ -115,20 +121,26 @@ function update_energies!(L::SpinLattice, qs::Vector{Tuple{Int,Int}})
     end
 end
 
+function update_energies_fast!(L::SpinLattice, qs::Vector{Tuple{Int,Int}}, s::Int)
+    for (i, j) in qs
+        L.bs[i,j].s == s ? L.bs[i,j].E -= 2 : L.bs[i,j].E += 2
+    end
+end
+
 m(ΔE, T) = minimum([1, exp(-ΔE / T)])
 
-function metropolis_step!(L::SpinLattice)
-    N = L.N
-    i, j = rand(1:N), rand(1:N)
+function metropolis_step!(L::SpinLattice; fast=false)
+    i, j = rand(1:L.N), rand(1:L.N)
     Ei = L.bs[i,j].E
     Ef = -Ei
     ΔE = Ef - Ei
     p = m(ΔE, L.T)
     u = rand()
     if u < p
+        flip!(L, i, j, fast=fast)
         L.flips += 1
-        update_energies!(L, i, j)
     end
+    L.steps += 1
 end
 
 end
